b00e13283238ea46d01dfdb32e08cce767112cec
amradelata <amradelata@gmail.com>
2019-08-18 10:44:47 +0200

    storing (prodacts) data and passing as props

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+    <productlistOne v-bind:prodacts="prodacts"/>
+    <productlistTwo v-bind:prodacts="prodacts"/>
   </div>
 </template>
 
+<script>
+import productlistOne from './components/productlistOne.vue';
+import productlistTwo from './components/productlistTwo.vue';
+export default {
+  components:{
+    productlistOne,
+    productlistTwo
+  },
+  data(){
+    return{
+      prodacts:[
+        {name: 'banana skin', price: 20},
+        {name: 'shiny star', price: 40},
+        {name: 'green shells', price: 60},
+        {name: 'red shells', price: 80}
+      ]
+    }
+  }
+}
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body{
+    font-family: Ubuntu;
+    color: #555;
 }
 </style>
